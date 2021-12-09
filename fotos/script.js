e3c8956161f661fd9f3c410911c06877ea66bc72
Blanca Murrieta <BMERIVERA@GMAIL.COM>
2021-12-09 00:25:40 -0700

    commit on 'MuseoWeb'

---
+++ fotos/script.js
@@ -0,0 +1,4 @@
+$("#carousel-example-generic .carousel-indicators li").hover(function(){
+  var goto = Number( $(this).attr('data-slide-to') );
+  $("#carousel-example-generic").carousel(goto);  
+});
